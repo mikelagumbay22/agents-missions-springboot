2379ff4833b18ee9e1d15e27e75defb95b97f71c
John Lagumbay <lagumbaj@sheridancollege.ca>
2026-07-18 01:10:35 -0400

    Setup index and controller

--- src/main/java/sheridan/lagumbaj/assignment3/controller/AgentMissionController.java
+++ src/main/java/sheridan/lagumbaj/assignment3/controller/AgentMissionController.java
@@ -1,4 +1,11 @@
 package sheridan.lagumbaj.assignment3.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class AgentMissionController {
+
+    @GetMapping({"/", "/index"})
+    public String showIndex() {
+        return "index";
+    }
 }
